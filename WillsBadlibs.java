2b5825264b57c67df14e8f99b3cba77851ce13d8
William Borges <borgesw14@gmail.com>
2016-02-03 17:31:59 -0500

    my madlibs story

---
+++ WillsBadlibs.java
@@ -0,0 +1,7 @@
+class WillsBadlibs {
+  String arr[] = {"a place", "an adjective", "a verb ending with -ing",
+    "a famous person", "a friend", "a present tense verb", "a room", "a musical instrument"};
+  String arrUser[] = new String[8];
+  
+
+}
